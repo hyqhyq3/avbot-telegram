ecdb882f7d00381ee1f9c0c16566929c1ca35ee0
hyq <i@hyq.me>
2017-01-16 11:25:31 +0000

    更新wiki地址

--- cmd/avbot/main.go
+++ cmd/avbot/main.go
@@ -22,7 +22,7 @@ func main() {
 4. 能否 Show 一段代码展示一下？ 
 另外请遵守如下的规则  
 * 请仔细阅读新人须知　http://avplayer.org/newbeefaq.html  
-* avplayer 社区建立了维基站点 http://wiki.avplayer.org , 有问题先到维基查找相关信息. 如果 wiki 上没有, 你获得了群友的回答,麻烦到维基上建立相关条目, 以方便后来人. wiki 公开编辑权限, 无需注册.  
+* avplayer 社区建立了维基站点 https://wiki.avplayer.org , 有问题先到维基查找相关信息. 如果 wiki 上没有, 你获得了群友的回答,麻烦到维基上建立相关条目, 以方便后来人. wiki 公开编辑权限, 无需注册.  
 * 问一些需要时间思考的问题请到论坛里发帖, 特别是 Boost 相关的问题. https://www.avboost.com.  
 * 长期潜水的人都会被强制清理. 伸手党会被立即清理. 十分钟内没有回答的人会被管理员请出群, 请不要无视机器人的通告. 管理员都是疯子,做好被虐待的准备,大胆发言.`))
 	bot.AddMessageHook(stat.New("stat.dat"))
