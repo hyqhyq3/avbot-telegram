6856d0b081afd77bc8b929020a330a800c0b0ef7
hyq <i@hyq.me>
2017-08-18 17:06:32 +0000

    增加发言者名字

--- ws/ws.go
+++ ws/ws.go
@@ -80,7 +80,7 @@ func (ws *WSChatServer) OnNewClient(c *websocket.Conn) {
 	for {
 		if err := websocket.JSON.Receive(c, msg); err == nil {
 			log.Println("received message ", msg)
-			tgmsg := tgbotapi.NewMessage(ws.chatId, msg.Data.Msg)
+			tgmsg := tgbotapi.NewMessage(ws.chatId, msg.Data.From + ": " + msg.Data.Msg)
 			ws.bot.Send(tgmsg)
 		}
 	}
